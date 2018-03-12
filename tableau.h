38c9d088b178d758b614b8d7395da95fe9f7ada7
Bruno <bruno.millet@ecl16.ec-lyon.fr>
2018-03-12 11:38:45 +0100

    commit ini

---
+++ tableau.h
@@ -0,0 +1,17 @@
+#ifndef TABLEAU_H
+#define TABLEAU_H
+#include "case.h"
+
+#include <vector>
+
+
+class Tableau
+{
+public:
+    Tableau();
+private:
+    int taille;
+    vector table;
+};
+
+#endif // TABLEAU_H
